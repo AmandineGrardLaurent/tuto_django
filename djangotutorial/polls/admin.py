a16c503c192a7583bd4d93555489fd4ee99e2495
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-24 08:37:47 +0100

    partie 2.2.1: ajout de la classe QuestionAdmin et ChoiceAdmin

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "pub_date")
+    list_filter = ["question_text", "pub_date"]
+    ordering = ["-pub_date"]
+    search_fields = ["question_text", "pub_date"]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("choice_text", "question", "votes")
+    list_filter = ["choice_text", "question", "votes"]
+    ordering = ["-question"]
+    search_fields = ["choice_text", "question__question_text", "votes"]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
