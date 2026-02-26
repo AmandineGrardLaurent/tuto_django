fdcb49a455665b8dc6440feb02394eb11107535a
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-02-26 15:47:51 +0100

    Partie 4.1 : tuto django partie 7 - personnalisation du formulaire d'administration - ajouts d'objets liés - personnalisation de la liste pour modification de l’interface d’administration

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.db.models import Sum
 from django.utils import timezone
@@ -32,6 +33,12 @@ class Question(models.Model):
 
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
 
     def __str__(self):
         """
